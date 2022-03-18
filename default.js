83c04a805464cc4f29f37ad40408e1824e03f3c2
ExtraSpadeTheA <satalice_puno@outlook.com>
2022-03-18 19:26:05 +0800

    3.18

--- default.js
+++ default.js
@@ -43,9 +43,11 @@ function musicwindow() {
     if (window.style.opacity==0) {
         window.style.animation = "mwin 0.5s forwards";
         window.style.opacity=1;
+        window.style.pointerEvents="auto";
     }else if(window.style.opacity==1){
         window.style.opacity=0;
         window.style.animation = "mwout 0.5s forwards";
+        window.style.pointerEvents="none";
     }
 
 }
