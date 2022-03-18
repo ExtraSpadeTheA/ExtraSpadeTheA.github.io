7d08bdf46a0d339f54a5ed5c331d50be428e6d51
ExtraSpadeTheA <satalice_puno@outlook.com>
2022-03-18 11:36:41 +0800

    nothing

---
+++ default.js
@@ -0,0 +1,39 @@
+function fade(){
+    var content=document.getElementById("fadecontent");
+    if(content.getBoundingClientRect().top<528){
+        content.style.animation="fadeIn 1s ";
+        content.style.opacity=1;
+    }else if(content.getBoundingClientRect().top>=528){
+        content.style.opacity=0;
+        content.style.animation="fadeOut 1s ";
+    }
+
+}
+
+function tohome(){
+    var page=document.getElementById("home");
+    page.style.animation="none";
+    setTimeout(() => {
+        page.style.animation="fadeIn 1s";
+    }, 0);
+}
+
+function DNshift(){
+    var shift=document.getElementById("rightmask");
+    if(shift.style.opacity==0){
+        shift.style.animation="backIn 1s";
+        shift.style.opacity=1;
+    }else if(shift.style.opacity==1){
+        shift.style.animation="backOut 1s";
+        shift.style.opacity=0;
+    }
+
+    var shift2=document.getElementById("leftmask");
+    if(shift2.style.opacity==0){
+        shift2.style.animation="backIn2 1s";
+        shift2.style.opacity=0.5;
+    }else if(shift2.style.opacity==0.5){
+        shift2.style.animation="backOut2 1s";
+        shift2.style.opacity=0;
+    }  
+}
